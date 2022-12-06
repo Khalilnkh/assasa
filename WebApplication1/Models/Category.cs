30384a9822b1f1cafd1fd37ffd85507acfa89833
Khalilnkh <khalilnkh@code.edu.az>
2022-12-06 11:49:25 +0400

    updated

--- WebApplication1/Models/Category.cs
+++ WebApplication1/Models/Category.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication1.Models
 {
@@ -15,6 +17,8 @@ namespace WebApplication1.Models
         public string Image { get; set; }
         public Nullable<int> ParentId { get; set; }
         public bool IsMain { get; set; }
+        [NotMapped]
+        public IFormFile[] File { get; set; }
         public Category Parent{ get; set; }
         public IEnumerable<Category > Children { get; set; }
         public IEnumerable<Product>products { get; set; }
